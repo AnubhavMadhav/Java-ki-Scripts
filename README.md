01f7c18b3cca4bd463055ea87d523003b3297bfd
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2021-04-30 21:27:51 +0530

    25 percent completed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Java-ki-Scripts
 Hue Hue Hue
+
+Nothing related to JAVA here! ;)
---
+++ index.js
@@ -0,0 +1,21 @@
+    // 1. Ways to print in JavaScript
+    // console.log("Hello World!")
+    // alert("Hey!")
+    // document.write('Hi Anubhav')
+
+    // 2. JavaScript Console API
+    // console.log("Heyyaaa!", 4 + 6, "Okay!")
+    // console.warn("This is warning")
+    // console.error("This is an error")
+
+    // 3. JavaScript Variables
+    // What are Variables? - Containers to store data values
+    var number1 = 34
+    var number2 = 56
+    console.log(number1 + number2)
+
+    /*
+        Multi
+        Line
+        Comment
+    */
